66623421037ec6486e758eb6dea8f9e673c3b211
LeChat76 <cedric.delauney@gmail.com>
2022-12-08 17:21:14 +0100

    code de récupération des valeurs terminé

--- script.py
+++ script.py
@@ -20,42 +20,68 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 ########## Data extraction ##########
 
+## values in 'td' will be used 4 times
+## so I declared "globally"
+## [0] universal product code
+## [2] price excluding tax
+## [3] price including tax
+## [5] number of books available
+td = soup.find_all('td')
+
+## values for a href will be used 2 times
+## so I declared "globally"
+ahref = soup.find_all("a")
+
+
+
+
 # product_page_url
+print("Product page URL : " + URL)
 
 # universal_product_code(upc)
-upc = soup.find_all('td')
-upc = (upc[0].string)
+upc = (td[0].string)
 print("UPC : " + upc)
 
 # title
-Title = (soup.find_all("li", class_="active"))
-Title = (Title[0].string) # pourquoi devoir faire ça?!? Titles.string ne fonctionne pas directement ???
-print("Titre : " + Title)
+## title stored on the first class active found
+title = (soup.find("li", class_="active"))
+title = (title.string)
+print("Title : " + title)
 
 # price_including_tax
-pit = soup.find_all('td')
-pit = (pit[3].string)
+pit = (td[3].string)
 print("Price including tax : " + pit)
 
 # price_excluding_tax
-pet = soup.find_all('td')
-pet = (pet[2].string)
+pet = (td[2].string)
 print("Price excluding tax : " + pet)
 
 # number_available
-available = soup.find_all('td')
-available = (available[5].string)
-print(available)
+available = (td[5].string)
+print("Number available : " + available)
 
 # product_description
-desc = soup.find("meta", attrs={"name":"description"})
+desc = str(soup.find("meta", attrs={"name":"description"}))
+desc=desc[20:-31]
+print("Description : " + desc)
 
 # category
+## looking for "category/books/" on ahref list
+for cat in ahref:
+    if "category/books/" in str(cat):
+        cat = cat.string
+        print("Category : " + cat)
 
 # review_rating
+rating = (td[6].string)
+print("Review rating : " + rating)
 
-# mage_url
-
-
-
+# image_url
+## find class item active (only one on all the html)
+## and looking for img url by searching ../../ at the beginning and jpg at the end
+image = str(soup.find(class_="item active"))
+pos1 = image.find("../../") + 6
+pos2 = image.find("jpg",pos1) + 3
+subString = ("http://books.toscrape.com/" + (image[pos1 : pos2]))
+print("Image URL : " + subString)
 
